fb363f780480107944ceb53bdde093863d6505f5
Zachary Becker <zrbecker@gmail.com>
2015-07-08 20:45:26 -0700

    forgot file

---
+++ GitWebService/GitWebServiceStatus.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Runtime.Serialization;
+
+namespace GitWebService
+{
+    [DataContract]
+    public enum GitWebServiceStatus
+    {
+        [EnumMember]
+        Success,
+
+        [EnumMember]
+        InvalidRequest,
+
+        [EnumMember]
+        ServerError
+    }
+}
